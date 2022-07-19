4858a889f83a2d9887891253734b198703135ab7
Roselie M <rose3001@global.chainsys.com>
2022-07-19 16:48:04 +0530

    assignment2 commit

---
+++ DemoProject/src/com/chainsys/assignment2/shopmanagement/test/BillingMgmtTest.java
@@ -0,0 +1,14 @@
+package com.chainsys.assignment2.shopmanagement.test;
+
+import com.chainsys.assignment2.shopmanagement.BillingMgmt;
+
+public class BillingMgmtTest {
+
+	public static void main(String[] args) {
+		BillingMgmt bm = new BillingMgmt();
+		
+		bm.getinfo();
+
+	}
+
+}
