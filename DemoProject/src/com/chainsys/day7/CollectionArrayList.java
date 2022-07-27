70ec506d8c2e27bbe756f44843c20d630ffc0c1b
Roselie M <rose3001@global.chainsys.com>
2022-07-27 16:49:40 +0530

    assignment updated

--- DemoProject/src/com/chainsys/day7/CollectionArrayList.java
+++ DemoProject/src/com/chainsys/day7/CollectionArrayList.java
@@ -1,10 +1,13 @@
 package com.chainsys.day7;
 
 import java.util.ArrayList;
+import java.util.LinkedList;
 
 public class CollectionArrayList {
 
 	public static void main(String args[]) {
+		//Array list
+		System.out.println("Array List");
 		ArrayList<String> list1 = new ArrayList<String>();  
 		ArrayList<String> list2 = new ArrayList<String>();  
 		list1.add("Apple");
@@ -20,5 +23,19 @@ public class CollectionArrayList {
 		list1.equals(list1);
 		System.out.println(list1.equals(list2));
 		System.out.println(list1.get(0));
+		
+		//Linked list
+		System.out.println("Linked List");
+		LinkedList<String> flowers1= new LinkedList<String>();
+		flowers1.add(0, "Rose");
+		flowers1.addLast("Lily");
+		LinkedList<String> flowers2 = (LinkedList<String>) flowers1.clone();
+		System.out.println(flowers2);
+		System.out.println(flowers1.element());
+		System.out.println(flowers1.getClass());
+		System.out.println(flowers1.peek());
+		System.out.println(flowers1.peekLast());
+		System.out.println(flowers1.size());
+		System.out.println(flowers1.isEmpty());
 	}
 }
