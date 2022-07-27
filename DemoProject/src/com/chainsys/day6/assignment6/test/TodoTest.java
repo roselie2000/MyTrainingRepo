3bcf8e39801fe257f7bee3c9b96ed80d7eefb1c5
Roselie M <rose3001@global.chainsys.com>
2022-07-27 16:22:52 +0530

    assignment updated

--- DemoProject/src/com/chainsys/day6/assignment6/test/TodoTest.java
+++ DemoProject/src/com/chainsys/day6/assignment6/test/TodoTest.java
@@ -10,28 +10,32 @@ import com.chainsys.day6.assignment6.TodoAppValidator;
 public class TodoTest {
 
 	public static void main(String args[]) {
-		TodoApp tp = new TodoApp();
-		TodoAppValidator tpv = new TodoAppValidator();
+		TodoApp tp = new TodoApp();//object creation for TodoApp class
+		TodoAppValidator tpv = new TodoAppValidator();//object creation for TodoAppValidator class
 		Scanner sn = new Scanner(System.in);
 		
+		//getting name from user
 		System.out.println("Enter your name :");
 		String name = sn.next();
 		String validName = tpv.nameValidation(name);
 		tp.setName(validName);
 		System.out.println(tp.getName());
 		
-		System.out.println("Enter your name :");
+		//getting username from user
+		System.out.println("Enter your username :");
 		String user = sn.next();
 		String validUser = tpv.userNameValidation(user);
 		tp.setUserName(validUser);
 		System.out.println(tp.getUserName());
 		
-		System.out.println("Enter your name :");
+		//getting password from user
+		System.out.println("Enter your password :");
 		String pwd = sn.next();
 		String validPwd = tpv.passwordValidation(pwd);
 		tp.setPassword(validPwd);
 		System.out.println(tp.getPassword());
 		
+		//Add task 
 		ArrayList<String> task = new ArrayList<String>();  
 		System.out.println("Enter No of Tasks :");
         int n = sn.nextInt();
@@ -41,6 +45,7 @@ public class TodoTest {
             task.add(sn.next());
         }
         
+        //print the task
         System.out.println("The Tasks are :\n");
         System.out.println(task);
 		
